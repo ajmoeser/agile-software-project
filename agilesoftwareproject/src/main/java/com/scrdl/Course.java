1de0f7d036b7564787ef43f46c06736b56901d55
Alexander Moeser <amoeser@fas.harvard.edu>
2016-11-09 14:51:21 -0600

    simple course repository

---
+++ agilesoftwareproject/src/main/java/com/scrdl/Course.java
@@ -0,0 +1,57 @@
+package com.scrdl;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String code;
+    private String name;
+    private String taughtBy;
+    private int courseLevel;
+    private String style;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaughtBy() {
+        return taughtBy;
+    }
+
+    public void setTaughtBy(String taughtBy) {
+        this.taughtBy = taughtBy;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.name = style;
+    }
+}
+
