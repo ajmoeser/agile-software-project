e4e49c6de2d54002e67a44c7b61d5cbb85a73738
Alexander Moeser <amoeser@fas.harvard.edu>
2016-11-10 17:30:31 -0600

    added thymeleaf basics and test web page (no design)

--- agilesoftwareproject/src/main/java/com/scrdl/IndexController.java
+++ agilesoftwareproject/src/main/java/com/scrdl/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/")
     String index(){
         return "index";
     }
